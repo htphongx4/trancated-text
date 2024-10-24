79d0d9068c33ea5aeefffb8f49e5531c41f41cd7
phonght <phonght@smartosc.com>
2024-10-24 14:55:12 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Trancated } from './Trancated'
 
 function App() {
-  const shortText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
   const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`
   return (
     <>
